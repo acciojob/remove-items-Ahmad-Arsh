22dd954edda5a30264138804ec4525bbfeb7cda6
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-04-19 21:47:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let colorSelect = document.querySelector('#colorSelect');
+let removeButton = document.querySelector('#remove-item');
+
+removeButton.addEventListener('click', function() {
+    let selectedOption = colorSelect.options[colorSelect.selectedIndex];
+    colorSelect.removeChild(selectedOption);
+});
